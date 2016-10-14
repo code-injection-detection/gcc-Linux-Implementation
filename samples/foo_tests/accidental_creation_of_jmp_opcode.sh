272753769b251ba4e629375e1066945cec7750c8
Konstantinos Mitropoulos (Kostas) <mitros123@hotmail.com>
2016-10-14 12:37:42 +0300

    Moved "accidental creation of jmp opcode.sh" and "obj_to_asm.sh" to the foo_tests directory

--- samples/foo_tests/accidental_creation_of_jmp_opcode.sh
+++ samples/foo_tests/accidental_creation_of_jmp_opcode.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 #script to prove that accildental generation of jmp <number of nops> opcode will result in error
+#run it and wait. At less than 30 iterations, the error should appear. It's a matter of probability. 
 #Now the problem is fixed though
+cd ..
 for ((i=1;i<=10000;i++)); do
 
 	echo $i
