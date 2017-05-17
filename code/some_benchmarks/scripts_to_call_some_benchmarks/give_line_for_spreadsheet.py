7b42fdc44f5b037c27c47bd674aacfc09776f4f1
Konstantinos Mitropoulos <mitros123@hotmail.com>
2017-05-17 19:47:58 +0300

    Added more benchmarks and graphs to the spreadsheet

--- code/some_benchmarks/scripts_to_call_some_benchmarks/give_line_for_spreadsheet.py
+++ code/some_benchmarks/scripts_to_call_some_benchmarks/give_line_for_spreadsheet.py
@@ -3,28 +3,30 @@
 import sys
 
 #gives a line of benchmarks  can be put into a spreadsheet easily
-name_of_file="./calc_determinant_sec_calc_det_11_squeezed_keys_with_padded_nops_maced/aggregated_results.txt"
+name_of_file="./calc_determinant_sec_calc_det_11_squeezed_keys_with_padded_nops_maced_data_blocks_same_size_as_code_blocks_and_multiple_of_16/aggregated_results.txt"
 
 file_src=open(name_of_file,'r')
 src_lines= file_src.readlines()
 
 var_code_sizes=[1,7,10,20,30,39]
-fixed_size_sizes=[20,55,71,75]
+#fixed_size_sizes=[20,55,71,75]
+fixed_size_sizes=[25,41,57,73] #for code block =data block
 
 types_of_bench=["NO_MACS","IGNORING_MACS","MACS_IGNORED_BUT_WE_FORCE_CODE_BLOCK_SPLIT","IGNORING_MACS_AT_LAST_MOMENT","DOING_EVERYTHING",
-				"CODE_CACHE=20_DATA_CACHE=20_ASSOC_2",
-				"CACHE=40_DATA_CACHE=40_ASSOC_2",
-				"CODE_CACHE=60_DATA_CACHE=60_ASSOC_2",
-				"CODE_CACHE=80_DATA_CACHE=80_ASSOC_2",
-				"CODE_CACHE=100_DATA_CACHE=100_ASSOC_2",
-				"CODE_CACHE=150_DATA_CACHE=150_ASSOC_2",
-				"CODE_CACHE=200_DATA_CACHE=200_ASSOC_2",
 				"CODE_CACHE=1_DATA_CACHE=1_ASSOC_DIRECT_MAPPED",
 				"CODE_CACHE=2_DATA_CACHE=2_ASSOC_DIRECT_MAPPED",
 				"CODE_CACHE=4_DATA_CACHE=4_ASSOC_DIRECT_MAPPED",
 				"CODE_CACHE=6_DATA_CACHE=6_ASSOC_DIRECT_MAPPED",
 				"CODE_CACHE=8_DATA_CACHE=8_ASSOC_DIRECT_MAPPED",
 				"CODE_CACHE=10_DATA_CACHE=10_ASSOC_DIRECT_MAPPED",
+				"CODE_CACHE=20_DATA_CACHE=20_ASSOC_2",
+				"CODE_CACHE=40_DATA_CACHE=40_ASSOC_2",
+				"CODE_CACHE=60_DATA_CACHE=60_ASSOC_2",
+				"CODE_CACHE=80_DATA_CACHE=80_ASSOC_2",
+				"CODE_CACHE=100_DATA_CACHE=100_ASSOC_2",
+				"CODE_CACHE=150_DATA_CACHE=150_ASSOC_2",
+				"CODE_CACHE=200_DATA_CACHE=200_ASSOC_2",
+				
 				]
 
 strs_not_present=[]
