bab6ca11b59ddd29d5647e5074f0d6d62db3899b
Konstantinos Mitropoulos <mitros123@hotmail.com>
2018-05-07 15:57:11 +0100

    Added the Dijkstra benchmarks in the spreadsheet

--- code/some_benchmarks/scripts_to_call_some_benchmarks/give_line_for_spreadsheet.py
+++ code/some_benchmarks/scripts_to_call_some_benchmarks/give_line_for_spreadsheet.py
@@ -3,16 +3,15 @@
 import sys
 
 #gives a line of benchmarks  can be put into a spreadsheet easily
-name_of_file="./calc_determinant_sec_calc_det_11_squeezed_keys_with_padded_nops_maced_data_blocks_same_size_as_code_blocks_and_multiple_of_16/aggregated_results.txt"
+name_of_file="./Dijkstra_Dijkstra_1400_nodes_stack_canary_3/aggregated_results.txt"
 
 file_src=open(name_of_file,'r')
 src_lines= file_src.readlines()
 
-var_code_sizes=[1,7,10,20,30,39]
 #fixed_size_sizes=[20,55,71,75]
-fixed_size_sizes=[25,41,57,73] #for code block =data block
+fixed_size_sizes=[16,32,48,64,80] #for code block =data block
 
-types_of_bench=["NO_MACS","IGNORING_MACS","MACS_IGNORED_BUT_WE_FORCE_CODE_BLOCK_SPLIT","IGNORING_MACS_AT_LAST_MOMENT","DOING_EVERYTHING",
+types_of_bench=["NO_MACS","IGNORING_MACS",
 				"CODE_CACHE=1_DATA_CACHE=1_ASSOC_DIRECT_MAPPED",
 				"CODE_CACHE=2_DATA_CACHE=2_ASSOC_DIRECT_MAPPED",
 				"CODE_CACHE=4_DATA_CACHE=4_ASSOC_DIRECT_MAPPED",
@@ -25,18 +24,18 @@ types_of_bench=["NO_MACS","IGNORING_MACS","MACS_IGNORED_BUT_WE_FORCE_CODE_BLOCK_
 				"CODE_CACHE=80_DATA_CACHE=80_ASSOC_2",
 				"CODE_CACHE=100_DATA_CACHE=100_ASSOC_2",
 				"CODE_CACHE=150_DATA_CACHE=150_ASSOC_2",
-				"CODE_CACHE=200_DATA_CACHE=200_ASSOC_2",
-				
+				"CODE_CACHE=200_DATA_CACHE=200_ASSOC_2"				
 				]
 
 strs_not_present=[]
 for type_of_bench in types_of_bench:
 	if (type_of_bench=="IGNORING_MACS"):
-		strs_not_present.append("AT_LAST_MOMENT")
+		pass		
+		#strs_not_present.append("AT_LAST_MOMENT")
 	else:
 		strs_not_present.append("RANDOM_STR_NEVER_PRESENT_ANYWHERE")
 
-
+'''
 for var_size in var_code_sizes:
 	lines_wanted=[]
 	var_size_str="VAR_SIZE_"+str(var_size)+":"
@@ -52,7 +51,8 @@ for var_size in var_code_sizes:
 				print(str_to_print,end=' ')
 				break
 	print("")
-		
+'''	
+	
 for fixed_size in fixed_size_sizes:
 	lines_wanted=[]
 	fixed_size_str="FIXED_SIZE_"+str(fixed_size)+":"
@@ -62,7 +62,7 @@ for fixed_size in fixed_size_sizes:
 			lines_wanted.append(line)
 	for i,type_of_bench in enumerate(types_of_bench):
 		for line in lines_wanted:
-			if (type_of_bench in line) and (strs_not_present[i] not in line):
+			if (type_of_bench in line): #and (strs_not_present[i] not in line):
 				number_in_sec=line.strip().split(":")[1].strip().split(" ")[0].strip()
 				str_to_print=str(number_in_sec).replace(".",",")
 				print(str_to_print,end=' ')
