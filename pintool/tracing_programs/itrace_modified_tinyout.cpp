c4b238cac8ec853fd466d9e78adcef91074103f3
Konstantinos Mitropoulos <mitros123@hotmail.com>
2018-11-29 00:37:01 +0000

    Changed the address numbers to be unsigned longs

--- pintool/tracing_programs/itrace_modified_tinyout.cpp
+++ pintool/tracing_programs/itrace_modified_tinyout.cpp
@@ -43,15 +43,15 @@ static unsigned long our_ins_addr;
 // and prints the IP
 /*
 VOID printip(VOID *ip, UINT64 insAddr,UINT64 insSize,std::string insDis) {
-    fprintf(trace, "IP=%p | INS_ADDR=%ld | I_SZ=%ld | DISAS=%s\n", ip,insAddr,insSize,insDis.c_str()); 
-    //fprintf(trace, "Instr_addr=%ld, Instr_size=%ld, Disassebled_str=%s\n",insAddr,insSize,insDis.c_str()); 
+    fprintf(trace, "IP=%p | INS_ADDR=%lu | I_SZ=%ld | DISAS=%s\n", ip,insAddr,insSize,insDis.c_str()); 
+    //fprintf(trace, "Instr_addr=%lu, Instr_size=%ld, Disassebled_str=%s\n",insAddr,insSize,insDis.c_str()); 
     fflush(trace);
 }
 */
 VOID printip(VOID *ip, UINT64 insAddr,UINT64 insSize) {
-    //fprintf(trace, "IP=%p | INS_ADDR=%ld | I_SZ=%ld | DISAS=%s\n", ip,insAddr,insSize,insDis.c_str()); 
-    //fprintf(trace, "Instr_addr=%ld, Instr_size=%ld, Disassebled_str=%s\n",insAddr,insSize,insDis.c_str()); 
-    fprintf(trace, "%ld|%ld\n",insAddr,insSize); 
+    //fprintf(trace, "IP=%p | INS_ADDR=%lu | I_SZ=%ld | DISAS=%s\n", ip,insAddr,insSize,insDis.c_str()); 
+    //fprintf(trace, "Instr_addr=%lu, Instr_size=%ld, Disassebled_str=%s\n",insAddr,insSize,insDis.c_str()); 
+    fprintf(trace, "%lu|%ld\n",insAddr,insSize); 
     fflush(trace);
 }
 
