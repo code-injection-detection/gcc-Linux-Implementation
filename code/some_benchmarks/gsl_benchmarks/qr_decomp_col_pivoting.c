13e03e825593dc4cb3997838cf0d325c1b05aa55
Konstantinos Mitropoulos <mitros123@hotmail.com>
2019-03-28 18:04:52 +0000

    Added benchmark for QR decomposition with column pivoting

--- code/some_benchmarks/gsl_benchmarks/qr_decomp_col_pivoting.c
+++ code/some_benchmarks/gsl_benchmarks/qr_decomp_col_pivoting.c
@@ -4,7 +4,7 @@
 #include <time.h>
 
 /*
-gcc qr_decomp.c  -lgsl -lgslcblas
+gcc qr_decomp_col_pivoting.c  -lgsl -lgslcblas
 */
 
 //solution for a*x=b
@@ -13,19 +13,24 @@ gcc qr_decomp.c  -lgsl -lgslcblas
 
 int main()
 {
-
 	int i,j;
+	int* signum;
 
     gsl_matrix *a;   
     gsl_vector *b;  
     gsl_vector *tau; 
     gsl_vector *x;
-    gsl_permutation * p;
+	gsl_vector *norm;
+	gsl_permutation * p;
 
     a = gsl_matrix_alloc(dimension, dimension); 
     b = gsl_vector_alloc(dimension); 
     tau = gsl_vector_alloc(dimension); 
     x = gsl_vector_alloc (dimension);
+	norm = gsl_vector_alloc (dimension);
+	p = gsl_permutation_alloc (dimension);
+	signum=malloc(sizeof(int));
+
 
 	srand(42); 
 	for (i=0;i<dimension;i++)
@@ -38,10 +43,10 @@ int main()
         gsl_vector_set(b, i, (rand()%15)+4); 
 	}  
 
-    gsl_linalg_QR_decomp(a,tau);
-    gsl_linalg_QR_solve(a,tau,b,x);
+    gsl_linalg_QRPT_decomp(a,tau,p,signum,norm);
+    gsl_linalg_QRPT_solve(a,tau,p,b,x);
 
-	if (PRINT_RES)
+	if (PRINT_RES) 
 	{
 		printf ("x = \n");
 		gsl_vector_fprintf (stdout, x, "%g");
