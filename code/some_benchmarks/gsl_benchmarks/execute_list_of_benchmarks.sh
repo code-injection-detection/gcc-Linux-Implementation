cde659a816e58b6d2528deaf76ed5170a4e3593d
Konstantinos Mitropoulos <mitros123@hotmail.com>
2019-04-03 21:41:20 +0100

    Added a mockup script that benchmarks gsl benchmarks automatically

---
+++ code/some_benchmarks/gsl_benchmarks/execute_list_of_benchmarks.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+
+DIR_WITH_GSL_BENCHMARKS=`pwd`
+DIR_WITH_PINTOOL_AUTOMATE_SCRIPT="${DIR_WITH_GSL_BENCHMARKS}/../../../pintool/"
+FLAGS=" -lgsl -lgslcblas -lm "
+files_to_compile=( "eigenvalues_eigenvectors_4th_order_hilbert_matrix_doubles.c" "qr_decomp_col_pivoting_doubles.c" "lu_decomp_doubles.c" "qr_decomp_doubles.c" "matrix_multiplication_floats.c" "matrix_multiplication_doubles.c" )
+
+
+for fl in "${files_to_compile[@]}";
+do
+    EXEC_NAME="${fl/.c/}"
+    gcc "${DIR_WITH_GSL_BENCHMARKS}/${fl}" -o "/tmp/${EXEC_NAME}" $FLAGS
+    cd ${DIR_WITH_PINTOOL_AUTOMATE_SCRIPT}
+    ./automate.sh tiny 1 1 fifo "/tmp/${EXEC_NAME}" "${EXEC_NAME}benchmark"
+    cd ${DIR_WITH_GSL_BENCHMARKS}
+done
+
+
